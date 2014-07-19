3753ccea47e873d0f049bfd5a3513e2327d546ee
volvet <volvet2002@gmail.com>
2014-07-19 17:12:50 +0800

    add button click listener

--- vsudoku/src/com/volvet/vsudoku/MainActivity.java
+++ vsudoku/src/com/volvet/vsudoku/MainActivity.java
@@ -2,17 +2,22 @@ package com.volvet.vsudoku;
 
 import com.volvet.vsudoku.R;
 
+import java.util.List;
+import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private SudokuGame  mGame = new SudokuGame();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		createInputListener();
 	}
 
 	@Override
@@ -24,7 +29,20 @@ public class MainActivity extends Activity {
 	
 	
 	protected void createInputListener() {
+		List<Button> btnList = new ArrayList<Button>();	
 		
+		btnList.add((Button)findViewById(R.id.button_1));
+		btnList.add((Button)findViewById(R.id.button_2));
+		btnList.add((Button)findViewById(R.id.button_3));
+		btnList.add((Button)findViewById(R.id.button_4));
+		btnList.add((Button)findViewById(R.id.button_5));
+		btnList.add((Button)findViewById(R.id.button_6));
+		btnList.add((Button)findViewById(R.id.button_7));
+		btnList.add((Button)findViewById(R.id.button_8));
+		btnList.add((Button)findViewById(R.id.button_9));
+		btnList.add((Button)findViewById(R.id.button_c));
+		
+		mGame.createInputListener(btnList);
 	}
 
 }
--- vsudoku/src/com/volvet/vsudoku/SudokuGame.java
+++ vsudoku/src/com/volvet/vsudoku/SudokuGame.java
@@ -1,9 +1,39 @@
 package com.volvet.vsudoku;
 
+import java.util.List;
+
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
 public class SudokuGame {
-	
+	static final String TAG = "SudokuGame"; 
+	static final int BTN_NUM = 10;
+	private OnClickListener  mBtnClickListener = new  OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			int num = (Integer)view.getTag();
+			
+			Log.i(TAG, "Click " + Integer.toString(num));
+		}
+	};
+ 	
 	public SudokuGame() {
 		
 	}
-
+	
+	public void createInputListener(List<Button> buttons) {
+		if( buttons.size() != BTN_NUM){
+			Log.e(TAG, "Create Input Listener failed");
+			return;
+		}
+		
+		int i;
+		for( i=0;i<buttons.size();i++ ){
+			Button btn = buttons.get(i);
+			btn.setTag(i+1);
+			btn.setOnClickListener(mBtnClickListener);
+		}
+	}
 }
